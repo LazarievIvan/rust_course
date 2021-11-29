0417a61efc5f88aec2b944c99ce9cbb7e71bed46
Ivan Lazariev <lazariev.ivan02@gmail.com>
2021-11-29 09:41:39 +0200

    Done task D

---
+++ D/control_flow_strings/target/debug/control_flow_strings.d
@@ -0,0 +1,1 @@
+C:\rust-course\tasks\D\control_flow_strings\target\debug\control_flow_strings.exe: C:\rust-course\tasks\D\control_flow_strings\src\main.rs
---
+++ D/control_flow_strings/target/debug/deps/control_flow_strings.d
@@ -0,0 +1,5 @@
+C:\rust-course\tasks\D\control_flow_strings\target\debug\deps\control_flow_strings.exe: src\main.rs
+
+C:\rust-course\tasks\D\control_flow_strings\target\debug\deps\control_flow_strings.d: src\main.rs
+
+src\main.rs:
