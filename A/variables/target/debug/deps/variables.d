df3e6c19c6217ce9c27fef7fbd34a7069f322d54
Ivan Lazariev <lazariev.ivan02@gmail.com>
2021-11-29 08:32:05 +0200

    Initial commit. Task A already Done

---
+++ A/variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+C:\rust-course\tasks\A\variables\target\debug\deps\variables.exe: src\main.rs
+
+C:\rust-course\tasks\A\variables\target\debug\deps\variables.d: src\main.rs
+
+src\main.rs:
---
+++ A/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\rust-course\tasks\A\variables\target\debug\variables.exe: C:\rust-course\tasks\A\variables\src\main.rs
