ab6635a160c40c78c9455d4f6d789086626d8d4d
Ivan Lazariev <lazariev.ivan02@gmail.com>
2021-12-06 20:38:14 +0200

    Task F done

---
+++ F/structs_traits/target/debug/deps/structs_traits.d
@@ -0,0 +1,5 @@
+C:\rust-course\tasks\F\structs_traits\target\debug\deps\structs_traits.exe: src\main.rs
+
+C:\rust-course\tasks\F\structs_traits\target\debug\deps\structs_traits.d: src\main.rs
+
+src\main.rs:
---
+++ F/structs_traits/target/debug/structs_traits.d
@@ -0,0 +1,1 @@
+C:\rust-course\tasks\F\structs_traits\target\debug\structs_traits.exe: C:\rust-course\tasks\F\structs_traits\src\main.rs
