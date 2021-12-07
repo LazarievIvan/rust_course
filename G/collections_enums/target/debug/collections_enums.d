aa65ed7e1a8df1342d499cdd65223f72a227dc3a
Ivan Lazariev <lazariev.ivan02@gmail.com>
2021-12-07 11:58:32 +0200

    Done task G

---
+++ G/collections_enums/target/debug/collections_enums.d
@@ -0,0 +1,1 @@
+C:\rust-course\tasks\G\collections_enums\target\debug\collections_enums.exe: C:\rust-course\tasks\G\collections_enums\src\main.rs
---
+++ G/collections_enums/target/debug/deps/getrandom-0d235ed7b87dc9c2.d
@@ -0,0 +1,11 @@
+C:\rust-course\tasks\G\collections_enums\target\debug\deps\getrandom-0d235ed7b87dc9c2.rmeta: C:\Users\lazar\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\lib.rs C:\Users\lazar\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\error.rs C:\Users\lazar\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\util.rs C:\Users\lazar\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\error_impls.rs C:\Users\lazar\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\windows.rs
+
+C:\rust-course\tasks\G\collections_enums\target\debug\deps\libgetrandom-0d235ed7b87dc9c2.rlib: C:\Users\lazar\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\lib.rs C:\Users\lazar\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\error.rs C:\Users\lazar\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\util.rs C:\Users\lazar\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\error_impls.rs C:\Users\lazar\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\windows.rs
+
+C:\rust-course\tasks\G\collections_enums\target\debug\deps\getrandom-0d235ed7b87dc9c2.d: C:\Users\lazar\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\lib.rs C:\Users\lazar\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\error.rs C:\Users\lazar\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\util.rs C:\Users\lazar\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\error_impls.rs C:\Users\lazar\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\windows.rs
+
+C:\Users\lazar\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\lib.rs:
+C:\Users\lazar\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\error.rs:
+C:\Users\lazar\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\util.rs:
+C:\Users\lazar\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\error_impls.rs:
+C:\Users\lazar\.cargo\registry\src\github.com-1ecc6299db9ec823\getrandom-0.2.3\src\windows.rs:
